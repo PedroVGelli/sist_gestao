e4a2d1bf55a408a80e47970d85d470b85066fd5d
PedroVGelli <kaicribeiro015@gmail.com>
2024-08-20 20:29:43 -0300

    Sistema de gestao de cortes sql

--- dados.sql
+++ dados.sql
@@ -3,14 +3,14 @@ CREATE TABLE usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    perfil ENUM('normal', 'administrador') DEFAULT 'normal',
-   
-    telefone INT(15) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    role ENUM('admin', 'user') DEFAULT 'user';
 );
 
-CREATE TABLE pedido (
-    id_prod INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    horario VARCHAR(30) NOT NULL);
-    
+CREATE TABLE agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    data_agendamento DATETIME NOT NULL,
+    servico VARCHAR(100),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
